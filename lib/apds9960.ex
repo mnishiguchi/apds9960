8dfe43673f60735442f4b558380bf3f03b8c24c5
Masatoshi N <nishiguchi.masa@gmail.com>
2022-01-30 17:05:53 -0500

    Initial

---
+++ lib/apds9960.ex
@@ -0,0 +1,18 @@
+defmodule APDS9960 do
+  @moduledoc """
+  Documentation for `APDS9960`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> APDS9960.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/apds9960_test.exs
@@ -0,0 +1,8 @@
+defmodule APDS9960Test do
+  use ExUnit.Case
+  doctest APDS9960
+
+  test "greets the world" do
+    assert APDS9960.hello() == :world
+  end
+end
